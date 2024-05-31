d6b0871a0c1548b416a397a274ac564da566040f
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-05-31 22:21:39 +0530

    chore: Add post-related components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostAddComponent } from './posts/post-add/post-add.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostEditComponent } from './posts/post-edit/post-edit.component';
+import { PostDetailsComponent } from './posts/post-details/post-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostAddComponent,
+    PostListComponent,
+    PostEditComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule,
