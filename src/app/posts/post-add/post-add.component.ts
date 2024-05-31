5700303f5e31824b1a2da54106ca585765c9d8d2
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-05-31 22:15:16 +0530

    chore: Add CLI analytics tracking ID

---
+++ src/app/posts/post-add/post-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-add.component.html',
+  styleUrl: './post-add.component.scss'
+})
+export class PostAddComponent {
+
+}
---
+++ src/app/posts/post-edit/post-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-edit.component.html',
+  styleUrl: './post-edit.component.scss'
+})
+export class PostEditComponent {
+
+}
